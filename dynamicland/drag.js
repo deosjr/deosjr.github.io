1ba1ab7dca7d5ff2a9ef1e3c05690d9de1fc3cd4
Sjoerd Dost <deosjr@gmail.com>
2025-05-29 19:27:09 +0200

    dynamicland: dragging pages onto table

--- dynamicland/drag.js
+++ dynamicland/drag.js
@@ -3,6 +3,17 @@ var offset = [0,0];
 var div;
 var isDown = false;
 
+table = document.getElementById("dynamicland");
+tableWidth = 500;
+tableHeight = 300;
+pageWidth = 100;
+pageHeight = 100;
+
+contains = function(element, x, y) {
+	rect = element.getBoundingClientRect();
+	return x > rect.x && (x + pageWidth) < (rect.x + tableWidth) && y > rect.y && (y + pageHeight) < (rect.y + tableHeight) ;
+}
+
 div = document.getElementById("test");
 
 div.addEventListener('mousedown', function(e) {
@@ -26,8 +37,15 @@ div.addEventListener('mousemove', function(event) {
             y : event.clientY
     
         };
-        div.style.left = (mousePosition.x + offset[0]) + 'px';
-        div.style.top  = (mousePosition.y + offset[1]) + 'px';
+	x = mousePosition.x + offset[0];
+	y = mousePosition.y + offset[1];
+        div.style.left = x + 'px';
+        div.style.top  = y + 'px';
+	if ( contains(table, x, y) ) {
+		div.style.background = "red";
+	} else {
+		div.style.background = "white";
+	}
     }
 }, true);
 
@@ -54,7 +72,14 @@ div2.addEventListener('mousemove', function(event) {
             y : event.clientY
     
         };
-        div2.style.left = (mousePosition.x + offset[0]) + 'px';
-        div2.style.top  = (mousePosition.y + offset[1]) + 'px';
+	x = mousePosition.x + offset[0];
+	y = mousePosition.y + offset[1];
+        div2.style.left = x + 'px';
+        div2.style.top  = y + 'px';
+	if ( contains(table, x, y) ) {
+		div2.style.background = "blue";
+	} else {
+		div2.style.background = "white";
+	}
     }
 }, true);
